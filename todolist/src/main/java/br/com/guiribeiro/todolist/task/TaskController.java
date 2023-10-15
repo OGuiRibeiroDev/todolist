82888f6b01f78f5019a07e105179333cad1d133b
Guilherme Henrique Machado RIbeiro <100424449+Sh1inx@users.noreply.github.com>
2023-10-15 08:15:43 -0300

    Update TaskController.java

--- todolist/src/main/java/br/com/guiribeiro/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/guiribeiro/todolist/task/TaskController.java
@@ -1,8 +1,12 @@
 package br.com.guiribeiro.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +23,19 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskmodel, HttpServletRequest request)
+    public ResponseEntity create(@RequestBody TaskModel taskmodel, HttpServletRequest request)
     {
-    System.out.println("Chegou no controller");
     var idUser = request.getAttribute("idUser");
     taskmodel.setIdUser((UUID) idUser);
+
+
+    var currentDate = LocalDateTime.now();
+    if(currentDate.isAfter(taskmodel.getStartAt()))
+    {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A DATA DE INÍCIO DEVE SER MAIOR QUE A DATA ATUAL");
+    }
+
     var task = this.taskRepository.save(taskmodel);
-    return task;
+    return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
