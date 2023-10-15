09920f944c44d1cc66ee8533819a01249af14fc9
Guilherme Henrique Machado RIbeiro <100424449+Sh1inx@users.noreply.github.com>
2023-10-15 08:39:32 -0300

    Alterações

--- todolist/src/main/java/br/com/guiribeiro/todolist/task/ITaskRepository.java
+++ todolist/src/main/java/br/com/guiribeiro/todolist/task/ITaskRepository.java
@@ -3,8 +3,10 @@ package br.com.guiribeiro.todolist.task;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
     
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID>
 {
-
+    List<TaskModel> findByIdUser(UUID idUser);
 }
--- todolist/src/main/java/br/com/guiribeiro/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/guiribeiro/todolist/task/TaskController.java
@@ -1,13 +1,17 @@
 package br.com.guiribeiro.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,12 +34,34 @@ public class TaskController {
 
 
     var currentDate = LocalDateTime.now();
-    if(currentDate.isAfter(taskmodel.getStartAt()))
+    if(currentDate.isAfter(taskmodel.getStartAt()) || currentDate.isAfter(taskmodel.getEndAt()))
     {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A DATA DE INÍCIO DEVE SER MAIOR QUE A DATA ATUAL");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A DATA DE INÍCIO / TÉRMINO DEVE SER MAIOR QUE A DATA ATUAL");
+    }
+
+    if(taskmodel.getStartAt().isAfter(taskmodel.getEndAt()))
+    {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A DATA DE INÍCIO DEVE SER MENOR QUE A DATA DE TÉRMINO");
     }
 
     var task = this.taskRepository.save(taskmodel);
     return ResponseEntity.status(HttpStatus.OK).body(task);
     }
+
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request)
+    {
+    var idUser = request.getAttribute("idUser");
+    var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+    return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id)
+    {
+        taskModel.setId(id);
+        return this.taskRepository.save(taskModel);
+    }
+
 }
